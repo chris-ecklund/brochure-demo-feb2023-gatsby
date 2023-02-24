cb3c580864cd1831c1d09ab9ce3b06f338c92e71
chris-ecklund <christopher.ecklund.eng@gmail.com>
2023-02-24 11:24:24 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `brochure-demo-feb2023-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
